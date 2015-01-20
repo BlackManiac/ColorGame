e29528e5b53b0c30c4dde9bc7734eb52dd741646
vishnu.satis <vishnu.satis@kony.com>
2015-01-21 01:13:07 +0530

    The package containing Basic Android and Desktop App

---
+++ desktop/src/blackdevs/vishnu/Colors/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package blackdevs.vishnu.Colors.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import blackdevs.vishnu.Colors.ColorGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ColorGame(), config);
+	}
+}
